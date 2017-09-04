5141daae20e34d07df535c3598c81868aa046828
Maximillian Paterson <msp73@georgetown.edu>
2017-09-04 13:59:44 -0400

    Organization and HTML

--- app/models/unschedData.js
+++ app/models/unschedData.js
@@ -1,11 +1,11 @@
 //format for the patient info blocks, exports as unschedPatientArray
 var unschedPatientArray = [
 	{
-		patientFirstName: "Joe"
-		patientLastName: "Frew"
-		patientEmail: "Jfrew@test.com"
-		patientPhone: "301-555-9001"
-		patientAddress: "1234 Anyroad St, Anytown MD"
+		firstName: "Joe"
+		lastName: "Frew"
+		email: "Jfrew@test.com"
+		phone: "301-555-9001"
+		address: "1234 Anyroad St, Anytown MD"
 		apptDay: "01/01/01"
 		apptTime: "9:30"
 	}

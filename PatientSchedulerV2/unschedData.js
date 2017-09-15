bb8d91a31dd37d7d9ee423ae12e1d6195b46e674
dariusrafei <dariusrafei@gmail.com>
2017-09-14 22:37:33 -0400

    Add files via upload
    Version 2 HTML

---
+++ PatientSchedulerV2/unschedData.js
@@ -0,0 +1,14 @@
+//format for the patient info blocks, exports as unschedPatientArray
+var unschedPatientArray = [
+	{
+		patientFirstName: "Joe"
+		patientLastName: "Frew"
+		patientEmail: "Jfrew@test.com"
+		patientPhone: "301-555-9001"
+		patientAddress: "1234 Anyroad St, Anytown MD"
+		apptDay: "01/01/01"
+		apptTime: "9:30"
+	}
+];
+
+module.exports = unschedPatientArray;
